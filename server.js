39937c112241283cc77a6b585787cdf146ac2fd0
root <root@caveman-pc.localdomain>
2021-04-20 18:59:54 +0530

    initial

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const path = require('path');
-const cors = require('cors');
-// Cors 
+const path = require("path");
+const cors = require("cors");
+// Cors
 const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
+  origin: process.env.ALLOWED_CLIENTS.split(","),
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
+};
 
 // Default configuration looks like
 // {
@@ -18,21 +18,40 @@ const corsOptions = {
 //     "optionsSuccessStatus": 204
 //   }
 
-app.use(cors(corsOptions))
-app.use(express.static('public'));
+//dont touch this
+//CORS middleware
+var corsMiddleware = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); //replace localhost with actual host
+  res.header(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, PUT, PATCH, POST, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, X-Requested-With, Authorization"
+  );
 
-const connectDB = require('./config/db');
+  next();
+};
+
+app.use(corsMiddleware);
+
+//////////////////////////
+
+app.use(cors(corsOptions));
+app.use(express.static("public"));
+
+const connectDB = require("./config/db");
 connectDB();
 
 app.use(express.json());
 
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
-// Routes 
-app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'));
-app.use('/files/download', require('./routes/download'));
-
+// Routes
+app.use("/api/files", require("./routes/files"));
+app.use("/files", require("./routes/show"));
+app.use("/files/download", require("./routes/download"));
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
